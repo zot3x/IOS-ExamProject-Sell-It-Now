c7b1878b8762af1f5e7d6833ddd4b40daae6f6ca
Onepiece <onepiece@Onepieces-MacBook-Pro.local>
2017-03-19 13:09:11 +0100

    Initial Commit

---
+++ Sell It Now/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sell It Now
+//
+//  Created by Onepiece on 19/03/2017.
+//  Copyright © 2017 Onepiece. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
